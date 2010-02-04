67ab0305761df8047e0a19c763eeee39e682a891
Premasagar <p@dharmafly.com>
2010-02-03 19:01:16 -0500

    data.js

---
+++ data.js
@@ -0,0 +1,8 @@
+(function(){
+    var
+        scripts = document.getElementsByTagName('script'),
+        script = scripts.length ?
+            scripts[scripts.length - 1] :
+            null,
+        data = script.dataList;
+}());
