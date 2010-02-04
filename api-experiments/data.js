92153e0e4f4d62046bb1d1199e777fbd0d1e6e7a
Premasagar <p@dharmafly.com>
2010-02-04 07:15:53 -0500

    script data demo added

--- api-experiments/data.js
+++ api-experiments/data.js
@@ -4,26 +4,35 @@
         script = scripts.length ?
             scripts[scripts.length - 1] :
             null,
-        data = script.dataset || (function(){
-            var dataset = {};
-            $.each(script.attributes, function(attr){
-                if (attr.slice(0,5) === 'data-'){
-                    dataset[attr.slice(5)] = attr
+        dataset =
+            script.dataset ||
+            // .dataset is new in HTML5, so we have a fallback of the general attributes list, plucking out those attributes that are prefixed with "data-"
+            (function(){
+                for (
+                    var
+                        i=0,
+                        attrs=script.attributes,
+                        len=attrs.length,
+                        dataset={},
+                        attr;                        
+                     i < len;
+                     i++
+                ){
+                    attr = attrs[i];
+                    if (attr.name.slice(0,5) === 'data-'){
+                        dataset[attr.name.slice(5)] = attr.value;
+                    }
                 }
-                return attr.slice(0,5) === 'data-';
-            });
-            return dataset;
-        }());
+                return dataset;
+            }());
         
-        // .dataset is new in HTML5, it requires fallback of the general attributes list, which may contain unrelated attributes, using a lopp to cycle through script attributes, to find those that start with "data-sqwidget-";
-        if (!data){
-            data = $.map(script.attributes, function(attr){
-                return attr.slice(0,5) === 'data-sqwidget-';
-            });
+        
+        // **
+        
+        
+        var demo = '', i=0;
+        for (prop in dataset){
+            demo += '<br />' + prop + ': ' + dataset[prop];
         }
-        else {
-            data = $.map(data, function(key){
-                return key.slice(0,9) === 'data-sqwidget-';
-            });
-        }
+        document.getElementById('report').innerHTML = demo;
 }());
