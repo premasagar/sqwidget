a095563cdf8fdbbd8610c2898dd57a66c7f9bf73
Premasagar <p@dharmafly.com>
2010-02-04 19:02:29 -0500

    dataset() function to grab data- attribute hash

--- api-experiments/data.js
+++ api-experiments/data.js
@@ -1,38 +1,58 @@
 (function(){
-    var
-        scripts = document.getElementsByTagName('script'),
-        script = scripts.length ?
-            scripts[scripts.length - 1] :
-            null,
-        dataset =
-            script.dataset ||
-            // .dataset is new in HTML5, so we have a fallback of the general attributes list, plucking out those attributes that are prefixed with "data-"
+    /*
+    Returns the 'dataset' property of an element, or a read-only object that mimics it.
+
+    The 'dataset' property is new in HTML5. It is a hash of key-value pairs, based on the element's attributes that are prefixed with 'data-'.
+
+    See:
+    http://ejohn.org/blog/html-5-data-attributes/
+    http://dev.w3.org/html5/spec/Overview.html#custom
+
+    */
+
+    function dataset(elem){
+        return elem.dataset ||
             (function(){
                 for (
                     var
-                        i=0,
-                        attrs=script.attributes,
-                        len=attrs.length,
-                        dataset={},
-                        attr;                        
-                     i < len;
-                     i++
+                        dataset = {},
+                        attrs = elem.attributes,
+                        i = attrs.length,
+                        attr;
+                     i;
+                     i--
                 ){
-                    attr = attrs[i];
+                    attr = attrs[i-1];
                     if (attr.name.slice(0,5) === 'data-'){
                         dataset[attr.name.slice(5)] = attr.value;
                     }
                 }
                 return dataset;
             }());
+    }
+    
+    var
+        window = this,
+        document = window.document,
+        DOMContentLoaded,
+        isReady,
+        readyBound,
+        readyList = [],
         
+        scripts = document.getElementsByTagName('script'),
+        elem = scripts.length ?
+            scripts[scripts.length - 1] :
+            null,
+        data = elem ? dataset(elem) : null;
         
         // **
         
         
-        var demo = '', i=0;
-        for (prop in dataset){
-            demo += '<br />' + prop + ': ' + dataset[prop];
+        var demo = '', prop;
+        for (prop in data){
+            if (data.hasOwnProperty(prop)){
+                demo += '<br />' + prop + ': ' + data[prop];
+            }
         }
         document.getElementById('report').innerHTML = demo;
 }());
