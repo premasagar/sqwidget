acbe8d6a71fb7a69e0f2a6f5b091eff22c7d350a
Premasagar Rose <p@dharmafly.com>
2010-02-05 14:11:43 -0500

    JSON-px tool: now strips whitespace between HTML tags

--- tools/jsonpx.js
+++ tools/jsonpx.js
@@ -1,8 +1,27 @@
 // E.g. jsonpx(htmlTemplate, 'myFunc');
- 
-function jsonpx(html, callbackName){
+
+function jsonpx(html, callbackName, stripwhitespace){
     function escape(txt){
-        return txt.replace(/'/g, "\\'").replace(/(?=\n)/g, '\\');
+        var s = '[\\0\\t\\n\\v\\f\\r\\s]';
+        
+        if (stripwhitespace){
+            txt = txt
+                .replace(new RegExp('>' + s + '+<', 'g'), '><')
+                .replace(new RegExp('(<style[^>]*>)' + s + '+(.)', 'g'), '$1$2')
+                .replace(new RegExp('(<script[^>]*>)' + s + '+(.)', 'g'), '$1$2');
+        }
+        txt = txt
+            .replace(/'/g, "\\'")
+            .replace(/(?=\n)/g, '\\');
+            
+        if (stripwhitespace){
+            txt = txt
+                .replace(new RegExp('(;?)' + s + '+<\/script>' + s + '*', 'g'), '$1</script>')
+                .replace(new RegExp('(;?)' + s + '*<\/script>' + s + '+', 'g'), '$1</script>')
+                .replace(new RegExp('(}?)' + s + '+<\/style>' + s + '*', 'g'), '$1</style>')
+                .replace(new RegExp('(}?)' + s + '*<\/style>' + s + '+', 'g'), '$1</style>');
+        }
+        return txt;
     }
 
     return callbackName + "('" + escape(html) + "');";
