ec022288add602eb2e8ef42284d3d679539e04a7
Premasagar <p@dharmafly.com>
2010-02-06 18:49:02 -0500

    tightened json-px whitespace minification

--- tools/jsonpx.js
+++ tools/jsonpx.js
@@ -8,19 +8,16 @@ function jsonpx(html, callbackName, stripwhitespace){
             txt = txt
                 .replace(new RegExp('>' + s + '+<', 'g'), '><')
                 .replace(new RegExp('(<style[^>]*>)' + s + '+(.)', 'g'), '$1$2')
-                .replace(new RegExp('(<script[^>]*>)' + s + '+(.)', 'g'), '$1$2');
+                .replace(new RegExp('(<script[^>]*>)' + s + '+(.)', 'g'), '$1$2')
+                .replace(new RegExp(s + '+<\/script>' + s + '*', 'g'), '</script>')
+                .replace(new RegExp(s + '*<\/script>' + s + '+', 'g'), '</script>')
+                .replace(new RegExp(s + '+<\/style>' + s + '*', 'g'), '</style>')
+                .replace(new RegExp(s + '*<\/style>' + s + '+', 'g'), '</style>');
         }
         txt = txt
             .replace(/'/g, "\\'")
             .replace(/(?=\n)/g, '\\');
             
-        if (stripwhitespace){
-            txt = txt
-                .replace(new RegExp('(;?)' + s + '+<\/script>' + s + '*', 'g'), '$1</script>')
-                .replace(new RegExp('(;?)' + s + '*<\/script>' + s + '+', 'g'), '$1</script>')
-                .replace(new RegExp('(}?)' + s + '+<\/style>' + s + '*', 'g'), '$1</style>')
-                .replace(new RegExp('(}?)' + s + '*<\/style>' + s + '+', 'g'), '$1</style>');
-        }
         return txt;
     }
 
