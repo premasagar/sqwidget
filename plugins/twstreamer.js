a1f5f0c520f493cc3682ecd27c202ec1714f8d80
Premasagar <p@dharmafly.com>
2010-05-08 23:15:10 -0400

    adjusted stream settings

--- plugins/twstreamer.js
+++ plugins/twstreamer.js
@@ -11,6 +11,19 @@
     
     ///////////////////    
     
+(function(){
+
+    Sqwidget.plugin('twstreamer', function(sqwidget, widget, $){
+    var pathToSwf = Sqwidget.buildResourcePath(
+        '../../',
+        'plugins/',
+        'TwStreamFlash.swf',
+        ''
+    );
+    
+    
+    ///////////////////    
+    
 /*!*
 * TwStreamer
 *   by Raffi Krikorian, http://github.com/r/twstreamer
@@ -21,7 +34,7 @@
 
     license:
         opensource.org/licenses/mit-license.php
-        
+
     usage:
         jQuery.twstreamer
             .init()                     // set up Flash listener
@@ -31,9 +44,10 @@
                     console.log(tweet);
                 },
                 'track',                // api method
-                'love'                  // api query
+                'love',                 // api query
+                'some.domain.com'       // optional alternative host
             );
-        
+
         // some time later...
         jQuery.twstreamer
             .disconnect();
@@ -41,48 +55,53 @@
 
 (function($){
 
-    var 
+    var
         user,       // private username
         pass,       // private password
         streamer,   // container for Flash object
         api = {
             config: {
                 apiPath: '/1/statuses/filter.json',
-                
+
                 swf: {
                     //swf: 'TwStreamFlash.swf',
                     swf: pathToSwf,
                     width: 1,
                     height: 1
                 },
-                
+
                 containerId: 'twstreamer', // id for container div
                 globalHandler: 'streamEvent' // global variable for Flash object to call repeatedly, on pushing tweets
             },
-            
+
             init: function(){
                 streamer = $('<div id="'+ this.config.containerId + '"></div>')
                     .appendTo('body')
                     .flash(this.config.swf);
                 return this;
             },
-        
+
             credentials: function(username, password){
                 user = username;
                 pass = password;
                 return this;
             },
-        
-            connect: function(handler, method, q){
+
+            connect: function(handler, method, q, host){
                 var path = this.config.apiPath +
                     '?' + (method || 'track') +
-                    '=' + (q || 'love');
+                    '=' + (encodeURIComponent(q) || 'love');
                     
-                window[this.config.globalHandler] = handler;   
-                
+                host = host || 'stream.twitter.com';
+
+                window[this.config.globalHandler] = function(encodedTweet) {
+                  var decodedTweet = encodedTweet.replace(/%22/g, "\"").replace(/%5c/g, "\\").replace(/%26/g, "&").replace(/%25/g, "%");
+                  handler(decodedTweet);
+                };
+_(host);
                 if (path && user && pass){
                     streamer.flash(function(){
-                        this.ConnectToStream(path, user, pass);
+                        this.ConnectToStream(host, path, user, pass);
                     });
                     return this;
                 }
@@ -96,10 +115,18 @@
                 return this;
             }
         };
-        
+
     jQuery.twstreamer = api;
-        
+
 }(jQuery));
+
+        ///////////////////
+
+        return jQuery.twstreamer;
+    
+    }, '0.1.0', ['jquery, swfobject']);
+
+}());
 
         ///////////////////
 
