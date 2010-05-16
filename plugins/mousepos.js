293168c9b84f91c1459b33a61aaea7c927d5b80a
Premasagar <p@dharmafly.com>
2010-05-16 05:38:06 -0400

    mousepos.toArray; general cleanup

--- plugins/mousepos.js
+++ plugins/mousepos.js
@@ -1,8 +1,9 @@
 // track the mouse's position
 var mousepos = (function($){
-    var x = null
-    ,   y = null
-    ,   tracking = false;
+    var document = window.document,
+        x = null,
+        y = null,
+        tracking = false;
     
     function captureCoords(ev){
         x = ev.pageX;
@@ -10,94 +11,100 @@ var mousepos = (function($){
     }
     
     function startTracking(){
-        $(document).mouseMove(captureCoords);
+        $(document).mousemove(captureCoords);
     }
     
     function stopTracking(){
-        $(document).unbind('mouseMove', captureCoords);
+        $(document).unbind('mousemove', captureCoords);
     }
     
-    return function(doTracking){
-        if (doTracking === true && !tracking){
-            startTracking();
-            tracking = true;
-        }
-        else if (doTracking === false && tracking){
-            stopTracking();
-            tracking = false;
-        }
-        return
-            {   x:x
-            ,   y:y
-            ,   tracking:tracking
+    return $.extend(
+        function(doTracking){
+            if (doTracking === true && !tracking){
+                startTracking();
+                tracking = true;
             }
-        ;
-    };
+            else if (doTracking === false && tracking){
+                stopTracking();
+                tracking = false;
+            }
+            return {
+                x:x,
+                y:y,
+                tracking:tracking
+            };
+        },
+        {
+            toArray: function(){
+                var pos = this();
+                return [pos.x, pos.y];
+            }
+        }
+    );
 }(jQuery));
 
 
 // check if coordinates are within a target element
 var overTarget = (function($){
     function boundingBox(element, includeMargin){
-        var $elem = $(element)
-        ,   w = $elem.outerWidth(includeMargin
-        ,   h = $elem.outerHeight(includeMargin)
-        ,   offset = $elem.offset()
-        ,   x = offset.left + w
-        ,   y = offset.top + h
-        ;
+        var $elem = $(element),
+            offset = $elem.offset(),
+            w = $elem.outerWidth(includeMargin),
+            h = $elem.outerHeight(includeMargin);
         
-        return
+        
+        return {
                 // distance of top of element from top of document
-            {   top: offset.top
+                top: offset.top,
                 // distance of right of element from left of document
-            ,   right: offset.left + w
+                right: offset.left + w,
                 // distance of bottom of element from top of document
-            ,   bottom: offset.top + h
+                bottom: offset.top + h,
                 // distance of left of element from left of document
-            ,   left: offset.left
+                left: offset.left
             };
     }
     
     function within(coords, bounds){
-        return
-            (
-                coords.x >= bounds.left
-            &&  coords.x <= bounds.right
-            &&  coords.y >= bounds.top
-            &&  coords.y <= bounds.bottom
-            );
+        if ($.isArray(coords)){
+            coords = {x:coords[0], y:coords[1]};
+        }
+        return (
+            coords.x >= bounds.left &&
+            coords.x <= bounds.right &&
+            coords.y >= bounds.top &&
+            coords.y <= bounds.bottom
+        );
     }
 
     return function(coords, target, includeMargin){
         includeMargin = !!includeMargin;
-        return within(coords, boundingBox(target, includeMargin);
+        return within(coords, boundingBox(target, includeMargin));
     };
 }(jQuery));
 
 
 // callback a function, if mouse coordinates are within a target, or another function, if not
-function ifOverTarget
-    (   target
-    ,   eventName
-    ,   callbackOverTarget
-    ,   callbackNotOverTarget
-    )
-    {
-        callbackOverTarget = callbackOverTarget || function(){};
-        callbackNotOverTarget = callbackNotOverTarget || function(){};
-        mousepos(true);
-        $(elem).bind(eventName, function(){
-            if (overTarget(mousepos(), target){
-                callbackOverTarget.call(elem);
-            }
-            else {
-                callbackNotOverTarget.call(elem);
-            }
-            mousepos(false);
-        });
-        return ifOverTarget;
-    }
+function ifOverTarget(
+    target,
+    eventName,
+    callbackOverTarget,
+    callbackNotOverTarget
+){
+    callbackOverTarget = callbackOverTarget || function(){};
+    callbackNotOverTarget = callbackNotOverTarget || function(){};
+    mousepos(true);
+    $(elem).bind(eventName, function(){
+        if (overTarget(mousepos(), target)){
+            callbackOverTarget.call(elem);
+        }
+        else {
+            callbackNotOverTarget.call(elem);
+        }
+        mousepos(false);
+    });
+    return ifOverTarget;
+}
 
 ifOverTarget
     (placeholder, 'delayedPlaceholderMouseover', showWidget)
