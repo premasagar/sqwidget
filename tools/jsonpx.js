269b74544f783566376ecca504ff7ee1a72111ed
Premasagar Rose <p@dharmafly.com>
2010-05-24 19:59:30 -0400

    jsonpx tool cleanup

--- tools/jsonpx.js
+++ tools/jsonpx.js
@@ -1,25 +1,33 @@
 // E.g. jsonpx(htmlTemplate, 'myFunc');
 
-function jsonpx(html, callbackName, stripwhitespace){
-    function escape(txt){
+function jsonpx(html, callbackName, doStripwhitespace){
+    function stripwhitespace(txt){
         var s = '[\\0\\t\\n\\v\\f\\r\\s]';
-        
-        if (stripwhitespace){
-            txt = txt
-                .replace(new RegExp('>' + s + '+<', 'g'), '><')
-                .replace(new RegExp('(<style[^>]*>)' + s + '+(.)', 'g'), '$1$2')
-                .replace(new RegExp('(<script[^>]*>)' + s + '+(.)', 'g'), '$1$2')
-                .replace(new RegExp(s + '+<\/script>' + s + '*', 'g'), '</script>')
-                .replace(new RegExp(s + '*<\/script>' + s + '+', 'g'), '</script>')
-                .replace(new RegExp(s + '+<\/style>' + s + '*', 'g'), '</style>')
-                .replace(new RegExp(s + '*<\/style>' + s + '+', 'g'), '</style>');
-        }
-        txt = txt
-            .replace(/'/g, "\\'")
-            .replace(/(?=\n)/g, '\\');
-            
-        return txt;
+        return txt
+            .replace(new RegExp('>' + s + '+<', 'g'), '><')
+            .replace(new RegExp('(<style[^>]*>)' + s + '+(.)', 'g'), '$1$2')
+            .replace(new RegExp('(<script[^>]*>)' + s + '+(.)', 'g'), '$1$2')
+            .replace(new RegExp(s + '+<\/script>' + s + '*', 'g'), '</script>')
+            .replace(new RegExp(s + '*<\/script>' + s + '+', 'g'), '</script>')
+            .replace(new RegExp(s + '+<\/style>' + s + '*', 'g'), '</style>')
+            .replace(new RegExp(s + '*<\/style>' + s + '+', 'g'), '</style>');
     }
 
-    return callbackName + "('" + escape(html) + "');";
+    function escape(txt){
+        return txt
+            .replace(/'/g, "\\'")       // escape single quotes
+            .replace(/(?=\n)/g, '\\');  // escape end of lines
+    }
+    
+    html = escape(
+        doStripwhitespace ?
+            stripwhitespace(html) :
+            html
+    );
+    
+    if (callbackName){
+        html = callbackName + "('" + html + "');";
+    }
+
+    return html;
 }
