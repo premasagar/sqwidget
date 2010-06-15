b0624fa5dbffe90d840103477d87317a6b5575f1
Premasagar <p@dharmafly.com>
2010-06-15 17:29:12 -0400

    plugin.js added - a boilerplate plugin file; sandie.js - a plugin wrapper for sandie

---
+++ plugins/plugin.js
@@ -0,0 +1,19 @@
+'use strict';
+
+(function(){
+
+/////////////////////////////////////////
+
+
+
+
+/////////////////////////////////////////
+
+
+Sqwidget.plugin('myplugin', function(){
+    return plugin;
+}, '0.1.0', ['jquery']);
+
+}());
+
+/*jslint browser: true, devel: true, onevar: true, undef: true, eqeqeq: true, bitwise: true, regexp: true, strict: true, newcap: true, immed: true */
