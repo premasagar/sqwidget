08de4ae5da25326ad2bf7b3bffd2a575f72969d2
Premasagar Rose <p@dharmafly.com>
2013-09-05 00:38:26 +0100

    Add Sqwidget.ready() and getParameters()

--- sqwidget.js
+++ sqwidget.js
@@ -16,6 +16,49 @@ window.Sqwidget = (function(window, document){
 
 	/////
 
+
+	/* contentloaded.js by Diego Perini
+	 * http://javascript.nwbox.com/ContentLoaded/
+	 * https://github.com/dperini/ContentLoaded
+	 */
+	function contentLoaded(win, fn) {
+
+		var done = false, top = true,
+
+		doc = win.document, root = doc.documentElement,
+
+		add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+		rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+		pre = doc.addEventListener ? '' : 'on',
+
+		init = function(e) {
+			if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+			(e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+			if (!done && (done = true)) fn.call(win, e.type || e);
+		},
+
+		poll = function() {
+			try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+			init('poll');
+		};
+
+		if (doc.readyState == 'complete') fn.call(win, 'lazy');
+		else {
+			if (doc.createEventObject && root.doScroll) {
+				try { top = !win.frameElement; } catch(e) { }
+				if (top) poll();
+			}
+			doc[add](pre + 'DOMContentLoaded', init, false);
+			doc[add](pre + 'readystatechange', init, false);
+			win[add](pre + 'load', init, false);
+		}
+
+	}
+
+
+
+	/////
+
 	
 	function Sqwidget(){
 
@@ -32,6 +75,30 @@ window.Sqwidget = (function(window, document){
 		return document.querySelectorAll('[data-sqwidget]');
 	};
 	
+	Sqwidget.getParameters = function(element){
+		var paramString = element.getAttribute('data-sqwidget'),
+			// TODO: uses regex to allow backslash escaping of semicolons
+			keyValues = paramString.split(';'),
+			length = keyValues.length,
+			ret = {},
+			i, keyValue, key, value;
+
+		for (i=0; i<length; i++){
+			if (keyValues[i] !== ''){
+				// TODO: uses regex to allow backslash escaping of colons
+				keyValue = keyValues[i].split(':');
+				key = keyValue[0];
+				value = keyValue[1];
+				ret[key] = value;
+			}
+		}
+		return ret;
+	};
+
+	Sqwidget.ready = function(callback){
+		contentLoaded(window, callback);
+		return this;
+	};
 	
 	/////
 	
--- tests/tests.js
+++ tests/tests.js
@@ -19,22 +19,34 @@
     });
   });
 
-  describe('Initialise widgets', function () {
+  describe('Detect widgets', function () {
     it('should detect elements with a `data-sqwidget` attribute', function () {
-      var elements, i, length, found;
-
-      expect(Sqwidget.getElements).to.be.a('function');
-
-      elements = Sqwidget.getElements();
-      length = elements.length;
+      var elements = Sqwidget.getElements();
 
       expect(elements).to.be.an.instanceof(window.NodeList);
-      expect(length).to.eql(1);
+      expect(elements.length).to.eql(2);
       expect(elements[0]).to.eql(document.getElementById('test-1'));
+    });
+
+    it('should get parameters from the `data-sqwidget` attribute', function () {
+      var elements = Sqwidget.getElements(),
+          params1 = Sqwidget.getParameters(elements[0]),
+          params2 = Sqwidget.getParameters(elements[1]);
+
+      expect(params1).to.be.an.object;
+      expect(params2).to.be.an.object;
+      expect(Object.keys(params1).length).to.eql(0);
+      expect(Object.keys(params2).length).to.eql(1);
+      expect(params2.foo).to.eql('bar');
+    });
+
+    it('Sqwidget.ready() should run callback on DOM load', function (done) {
+      Sqwidget.ready(function(){
+        done();
+      });
+
+      // TODO: test when sqwidget.js included in HTML head, or above widgets
     });
   });
 
-  function notDone () {
-    assert.ok(false, 'Test not implemented'.toUpperCase());
-  }
 }(window, window.Sqwidget));
