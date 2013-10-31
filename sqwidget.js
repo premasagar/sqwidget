eb91f45d62f6cf389cf27959016e02415f3798e9
Simon Elliott <simon@papercreatures.com>
2013-10-31 11:03:08 +0000

    move to lodash + cdns

--- sqwidget.js
+++ sqwidget.js
@@ -5,10 +5,10 @@
     //Optional dependencies which aren't anonymous. AMD is crazy.
     //preloads: ['curl/plugin/css','curl/plugin/i18n', 'curl/plugin/domReady'],
     paths: {
-      underscore: { location: './app/lib/underscore-amd/underscore' },
+      underscore: { location: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.2.1/lodash.underscore.min.js' },
       backbone: { location: './app/lib/backbone-amd/backbone' },
-      jquery: { location: './app/lib/jquery/jquery' },
-      moment: { location: './app/lib/momentjs/moment' },
+      jquery: { location: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js' },
+      moment: { location: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.4.0/moment.min.js' },
     },
     preloads: ['i18n'],
     packages: {
