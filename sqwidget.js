fbe28a071e8dbc361ae436016d3b21a69cbab0de
Simon Elliott <simon@papercreatures.com>
2013-11-02 21:18:49 +0000

    build fixes

--- sqwidget.js
+++ sqwidget.js
@@ -7,22 +7,14 @@
     paths: {
       qwery: { location: '//cdnjs.cloudflare.com/ajax/libs/qwery/3.4.1/qwery.js' },
       underscore: { location: '//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.2.1/lodash.underscore.min.js' },
-      backbone: { location: './app/lib/backbone-amd/backbone' },
       bean: { location: '//cdnjs.cloudflare.com/ajax/libs/bean/1.0.3/bean.min.js' },
-      jquery: { location: '//cdnjs.cloudflare.com/ajax/libs/jquery/1.10.2/jquery.min.js' },
       moment: { location: '//cdnjs.cloudflare.com/ajax/libs/moment.js/2.4.0/moment.min.js' },
     },
     preloads: ['i18n'],
+    main: "app/main",
     packages: {
       i18n: { location: './app/plugins', main: 'i18n' },
       curl: { location: './app/lib/curl/src/curl/' },
-      sqwidget: { location: './app', main: 'main' }
     }
   });
-
-  curl(['sqwidget']).then(
-    //success
-    function(main) { },
-    function(err) { console.log("Sqwidget failed to start: " + err); }
-  );
 }());
