ecec9cbd1299f4de65538fbf56d2e61139d7c21a
Simon Elliott <simon@papercreatures.com>
2013-11-14 13:02:04 +0000

    update scaffold

--- grunt-scaffold/root/src/main.js
+++ grunt-scaffold/root/src/main.js
@@ -4,13 +4,11 @@ define([
   'css!compiled/css/main.css',
   'rv!templates/index.html'
 ], function(require, strings, css, template ) {
-  return {
-    Controller: function(opts) {
-      this.view = new Ractive({
-        el: opts.config.el,
-        template: template,
-        data: { message: strings.hello + " World" }
-      });
-    }
+  return function(opts) {
+    var view = new Ractive({
+      el: opts.el,
+      template: template,
+      data: { message: strings.hello + " World" }
+    });
   };
 });
