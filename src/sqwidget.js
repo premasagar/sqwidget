dbafd01e8632f567776d1be17c2dedc38ae110ae
Simon Elliott <simon@papercreatures.com>
2013-12-13 13:49:32 +0000

    rename

--- src/sqwidget.js
+++ src/sqwidget.js
@@ -1,8 +1,8 @@
 define(['require', './core'], function(require, Core) {
   var sqwidget = new Core();
-  if(!window._sqwidget) {
+  if(!window._sqwidgetCore) {
     //only clobber global if it doesn't exist
-    window._sqwidget = sqwidget;
+    window._sqwidgetCore = sqwidget;
   }
 
   require(['domReady!'], function() {
